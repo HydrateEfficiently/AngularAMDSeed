ae35b41ba033a3a8091898009f7dad21a5d3aca8
Michael Fry <michaelfry2002@gmail.com>
2015-10-17 11:03:01 +0100

    Tidy up

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 var http = require('http');
 var express = require('express');
-var bodyParser = require('body-parser');
 
 var port = process.env.port || 8005;
 
 var app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 app.use(express.static('.'));
 
 app.get('/', function (request, response) {
